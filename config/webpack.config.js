3101e39e04535f23559ae44dfc52da87920639cd
Vadim Storozhuk <vadim.storozhuk@man.com>
2019-05-20 17:47:24 -0400

    Guides -> Output Management

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,45 +1,21 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-  target: "web",
-  entry: './src/index.js',
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js'
+  },
   output: {
     path: path.resolve(__dirname, '../dist'),
-    filename: 'bundle.js'
+    filename: '[name].bundle.js'
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.(png|svg|jpe?g|gif)$/,
-        use: [
-          'file-loader'
-        ]
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [
-          'file-loader'
-        ]
-      },
-      {
-        test: /\.(csv|tsv)$/,
-        use: [
-          'csv-loader'
-        ]
-      },
-      {
-        test: /\.xml$/,
-        use: [
-          'xml-loader'
-        ]
-      }
-    ]
-  }
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      title: 'Output Management',
+    })
+  ]
 }
--- src/index.js
+++ src/index.js
@@ -1,33 +1,16 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.jpg';
-import Data from './data.xml';
-
-// import Style from './style.css';
-// console.log('Vadim Debug 01');
-// console.warn(Icon);
-// console.warn(Style.toString());
-console.log(Data);
+import printMe from './print.js';
 
 function component() {
   const element = document.createElement('div');
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
 
-  // Add the image to our existing div.
-  const myIcon = new Image();
-  myIcon.src = Icon;
-  element.appendChild(myIcon);
-
-  return element;
-}
-
-function lodashVersion() {
-  const element = document.createElement('div');
-  element.innerHTML = _.VERSION;
-
+  const btn = document.createElement('button');
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+  
+  element.appendChild(btn);
   return element;
 }
 
 document.body.appendChild(component());
-document.body.appendChild(lodashVersion());
